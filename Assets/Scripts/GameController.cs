e9bcddd0903c762348fd6e85e6ccf74b57f64fe6
Ian Salmons <ian.salmons+github@gmail.com>
2014-05-26 10:12:40 +0100

    Part 16 Ending thr game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,17 +11,34 @@ public class GameController : MonoBehaviour
 	public float waveWait;
 
     public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
     private int score;
 
 	void Start () {
         score = 0;
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
         UpdateScore ();
 		StartCoroutine( SpawnWaves ());
 	}
 
+    void Update() {
+        if (restart) {
+            if (Input.GetKeyDown (KeyCode.R)) {
+                Application.LoadLevel (Application.loadedLevel);
+            }
+        }
+    }
+
 	IEnumerator SpawnWaves () {
 		yield return new WaitForSeconds(startWait);
-		while(true){
+        while(true){
 			for (int i = 0; i < hazardCount; i++) {
 				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
@@ -29,6 +46,13 @@ public class GameController : MonoBehaviour
 				yield return new WaitForSeconds(spawnWait);
 			}
 			yield return new WaitForSeconds(waveWait);
+
+            if(gameOver) {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+
 		}
 	}
     public void AddScore(int newScoreValue) {
@@ -37,5 +61,11 @@ public class GameController : MonoBehaviour
     }
     void UpdateScore() {
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver() {
+        gameOver = true;
+        gameOverText.text = "Game Over!";
+
     }
 }
