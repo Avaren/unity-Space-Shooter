8388f6eed17b2181ac6567b324902e153e4c2acd
Ian Salmons <ian.salmons+github@gmail.com>
2014-05-26 09:30:13 +0100

    Part 14 Audio

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class DestroyByBoundary : MonoBehaviour {
 
 	void OnTriggerExit(Collider other) {
+        Debug.Log (other.name);
 		Destroy(other.gameObject);
 	}
 
